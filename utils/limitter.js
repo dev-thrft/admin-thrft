c8317db3704ad1b43bf01975a148b342c530699b
druloloy <dru.loloy@gmail.com>
2022-01-05 22:58:12 +0800

    [mod] Following the new product model
    New product model updated for Product Archive

--- utils/limitter.js
+++ utils/limitter.js
@@ -1,15 +1,11 @@
 const Exception = require('../utils/Exception');
 
-function Limitter(value, min, max, title, next) {
+exports.Limitter = (value, min, max, title, next) => {
     if (value < min || value > max)
-        return next(new Exception(`${title} must be between ${min}-${max}`, 401));
-}
-function ArrayLimitter(value, min, max, title, next) {
-    if (!Array.isArray(value) || value.length < min || value.length > max)
-        return next(new Exception(`${title} must be between ${min}-${max}`, 401));
-}
+        return next(new Exception(`${title} must be between ${min} and ${max}`, 401));
+};
 
-exports = {
-    Limitter,
-    ArrayLimitter
+exports.ArrayLimitter = (value, min, max, title, next) => {
+    if (!Array.isArray(value) || value.length < min || value.length > max)
+        return next(new Exception(`${title} must be between ${min} and ${max}`, 401));
 };
